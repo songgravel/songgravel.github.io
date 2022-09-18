3b629bfa41e9dfda04773a72b54c9b592b917b98
songgravel <songgravel@163.com>
2022-09-18 19:03:17 +0800

    update pintu test

---
+++ web-mobile/src/settings.7c1c1.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","test"],collisionMatrix:[[false],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/remote/scene/collect/init.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"09c14",main:"3c603"}};
